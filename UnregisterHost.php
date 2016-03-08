eacd5cd4465d651a3604e9934d6ad0d6fd706bb0
Nathan Fabian <ndfabian@gmail.com>
2016-03-07 22:06:10 -0700

    Cleaned up and handles gamename overlap between machines.

--- UnregisterHost.php
+++ UnregisterHost.php
@@ -1,15 +1,20 @@
 <?php
-require 'sql.conf.php';
-$db = mysql_connect ($sqlc["server"], $sqlc["user"], $sqlc["pass"]);
-if (!mysql_select_db ($sqlc["db"])) {
-	echo "Could not select database " . mysql_error ();
-	exit;
-}
-$query = "DELETE FROM MasterServer WHERE gameType='".$_REQUEST['gameType']."' AND gameName='".$_REQUEST['gameName']."';";
-$res = mysql_query ($query);
-if (!$res) {
-	echo "Could not execute query: " . mysql_error ();
-	exit;
-}
-mysql_close ($db);
+    require 'sql.conf.php';
+    $db = mysql_connect ($sqlc["server"], $sqlc["user"], $sqlc["pass"]);
+    if (!mysql_select_db ($sqlc["db"])) {
+        echo "Could not select database " . mysql_error ();
+        exit;
+    }
+    $query = "DELETE FROM MasterServer WHERE gameType='"
+        .mysql_real_escape_string($_REQUEST['gameType'])
+        ."' AND gameName='".mysql_real_escape_string($_REQUEST['gameName'])
+        ."' AND externalIP='".$_SERVER['REMOTE_ADDR']
+        ."';";
+    $res = mysql_query ($query);
+    if (!$res) {
+    	echo "Could not execute query: " . mysql_error ();
+    	exit;
+    }
+    mysql_close ($db);
+    print "succeeded";
 ?>
